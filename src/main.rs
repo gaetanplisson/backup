9b384c6abecf298fa8d5b0106568c9a9a874051f
Tatan <gaetan.plisson@gmail.com>
2023-12-19 09:45:26 +0100

    Add dependencies and implement file download functionality

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 use std::fs::File;
 use flate2::Compression;
 use flate2::write::GzEncoder;
+use futures::future::ErrInto;
+use opendal::raw::oio::Stream;
 use tar::Builder;
 use opendal::services::Webdav;
-use opendal::Operator;
+use opendal::{Operator, Entry};
 use opendal::raw::HttpClient;
 use tokio;
 use reqwest;
+use futures::stream::StreamExt;
+use tokio::io::AsyncReadExt;
 
 
 
-fn create_tar_gz() -> std::io::Result<()> {
-    let tar_gz = File::create("test.tar.gz")?;
+fn archive_builder() -> Option<Builder<GzEncoder<File>>> {
+    let tar_gz = match File::create("test.tar.gz"){
+        Ok(archive) => archive,
+        Err(_)=> return None
+    };
     let enc = GzEncoder::new(tar_gz, Compression::default());
-    let mut tar = Builder::new(enc);
-    tar.append_dir_all("test", "test")?;
-    tar.finish()?;
-    Ok(())
+    let mut tar: Builder<GzEncoder<File>> = Builder::new(enc);
+    //tar.append_dir_all("test", "test")?;
+    //tar.finish()?;
+    Some(tar)
 }
 
 // async fn connect_webdav() -> std::io::Result<Vec<opendal::Entry>> {
@@ -40,6 +47,35 @@ fn connect_webdav() -> Operator {
         Err(e) => panic!("Error: {}", e),
     };
     op
+}
+
+async fn download_files (op: Operator, path: &str)-> () {
+    
+    let mut entries_lister = op.lister_with(path)
+        .recursive(true).await.unwrap(); 
+
+    while let Some(entry) = entries_lister.next().await {
+        let mut reader = match entry {
+            Ok(entry) => {
+                println!("Entry: {:?}", entry);
+                let reader = match op.reader(path).await {
+                    Ok(r) => r,
+                    Err(e) => {println!("Error: {}", e); continue;},
+                };
+                reader
+            },
+            Err(e) => {println!("Error: {}", e); continue;},
+        };
+        let mut buffer: [u8; 1024] = [0 ; 1024]; // 1kb buffer, it may change nothing if more was attributed
+        
+        let mut tmpFile = File::create("tmpFile").unwrap();
+
+        while reader.read(&mut buffer).await.unwrap() != 0{
+            //write buffer into archive
+        };
+        
+    }
+    ()
 }
 
 #[tokio::main]
